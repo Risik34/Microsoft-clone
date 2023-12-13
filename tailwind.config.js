78118f83af4f782ff0c2aa9be87a4a3f9bd3c41a
risimn7@gmail.com <risimn7@gmail.com>
2023-12-13 14:10:25 +0530

    Styled features and product section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
  /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*"],
-  theme: {
-    extend: {},
+  theme: {theme: {
+                fontFamily:{
+                    'sans-serif':['Rethink Sans'],
+                    'serif':['Kalnia']
+                }
+                extend: {
+                    colors: {
+                        clifford: '#da373d',
+                    }
+                }
+            }
+    ,
   },
   plugins: [],
 }
