6dfd686da7264d6b82f3660140be8612edcbcafc
Risik34 <risimm7@gmail.com>
2023-12-13 15:41:36 +0530

    Reinitialized configs resolved all issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*"],
- theme: {
-                fontFamily:{
-                    'sans-serif':['Rethink Sans'],
-                    'serif':['Kalnia']
-                },
-                extend: {
-                    colors: {
-                        clifford: '#da373d',
-                    }
-                }
-            }
-    
+  theme: {
+    extend: {},
   },
   plugins: [],
 }
+
